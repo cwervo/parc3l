65845460cbf51c1dd1437d067be1dfe38a52e22d
Andres Cuervo <andrescuervor@gmail.com>
2018-03-12 06:40:24 -0700

    Get rid of top level vars

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import * as THREE from 'three';
 import {add} from './wow.rs';
 console.log(add(2, 3));
 
-var camera, scene, renderer;
-var geometry, material, mesh;
+let camera, scene, renderer;
+let geometry, material, mesh;
 
 init();
 animate();
